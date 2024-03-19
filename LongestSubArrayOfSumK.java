83757c246b594978a5199d36507dfb014448081c
saurabhsingh <saurabh623a@gmail.com>
2024-03-19 23:40:00 +0530

    Day-4

--- LongestSubArrayOfSumK.java
+++ LongestSubArrayOfSumK.java
@@ -1,2 +1,24 @@
 public class LongestSubArrayOfSumK {
+    int largestSubarray(int[] arr, int k) {
+        int max = 0, sum = 0, i = 0, j = 0;
+        while (j < arr.length) {
+            sum += arr[j];
+            if (sum < k) {
+                j++;
+            } else if (sum == k) {
+                max = Math.max(max, j - i + 1);
+                j++;
+            } else {
+                while (sum > k) {
+                    sum -= arr[i];
+                    i++;
+                    if (sum == k) {
+                        max = Math.max(max, j - i + 1);
+                    }
+                }
+                j++;
+            }
+        }
+        return max;
+    }
 }
